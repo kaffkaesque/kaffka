f3d835ce09000b7144150a4d2457e866268ec4f1
Finn Holme <finninde@stud.ntnu.no>
2018-11-08 18:32:23 +0100

    Add a better effect that can actually be used

--- cafe/work-work/workwork.js
+++ cafe/work-work/workwork.js
@@ -16,8 +16,17 @@ var frameTimeInterval = setInterval(timeAndFrameUpdater, msperframe);
 function workworkEffect (){
     ctx.save();
     ctx.clearRect(0, 0, c.width, c.height);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(150 * Math.cos(frame/50)  + 150,  150 * Math.sin(frame/50)  + 150, 100, 100);
+    for (var i =0 ; i<50; i++){
+        if (i%2 ==0){
+            ctx.fillStyle = 'green';
+            ctx.fillRect(i*10, (50)*(Math.sin(i*3+(frame/40)) + 0.5)+30,  5,5);
+        }
+        else{
+            ctx.fillStyle='red';
+            ctx.fillRect(i*10, (50)*(Math.sin(i*3+(frame/40)) + 0.5)+30,  5,5);
+        }
+    }
+    //ctx.fillRect(150 * Math.cos(frame/50)  + 150,  150 * Math.sin(frame/50)  + 150, 100, 100);
 }
 
 var updt = setInterval(workworkEffect, msperframe);
